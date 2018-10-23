36414ec8f12c62cf1c5c9fd56a0bc8adef8d1e36
fintechilla <shwalerys@gmail.com>
2018-10-24 00:31:06 +0200

    Module 14.4 - @RequestMapping

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -15,14 +15,18 @@ public class TaskController {
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long id) {
         return new TaskDto(1L, "testTitle", "testContent");
     }
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long id) {
     }
+    @RequestMapping(method = RequestMethod.PUT, value = "updateDto")
     public TaskDto updateDto(TaskDto taskDto) {
         return new TaskDto(1L, "editedtestTitle", "editedTestContent");
     }
+    @RequestMapping(method = RequestMethod.POST, value = "createDto")
     public void createDto(TaskDto taskDto){
 
     }
