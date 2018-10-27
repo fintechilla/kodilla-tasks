3f3584a64fadf37dd0039dda2b821afeef4017ea
fintechilla <shwalerys@gmail.com>
2018-10-27 23:29:11 +0200

    Module 14.4 - TaskController - modified creteTask and updateTask

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,9 +1,10 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,23 +12,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
         return new ArrayList<>();
     }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long id) {
         return new TaskDto(1L, "testTitle", "testContent");
     }
+
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(Long id) {
-    }
-    @RequestMapping(method = RequestMethod.PUT, value = "updateDto")
-    public TaskDto updateDto(TaskDto taskDto) {
+    public void deleteTask(Long id) { }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "editedtestTitle", "editedTestContent");
     }
-    @RequestMapping(method = RequestMethod.POST, value = "createDto")
-    public void createDto(TaskDto taskDto){
 
+    @RequestMapping(method = RequestMethod.POST, value = "createTask")
+    public String createTask(TaskDto taskDto){
+        return "Hello, this is createTask!";
     }
 }
