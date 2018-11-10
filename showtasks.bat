685e2d6706235f1e06fe986e88a496eb7eca77be
fintechilla <shwalerys@gmail.com>
2018-11-10 18:44:02 +0100

    Module 17.4 Shell Script - the ERRORLEVEL generated by runcrud is picked up by showtasks and next command from showtasks is executed and start of Spring is holted

---
+++ showtasks.bat
@@ -0,0 +1,15 @@
+echo starting... showtasks.bat
+echo running... runcrud.bat
+call runcrud.bat
+
+if %ERRORLEVEL% == 0 goto startbrowser
+echo There was a problem with runcrud.bat
+goto end
+
+:startbrowser
+START chrome "https://www.onet.pl"
+echo openining webbrowser successfull
+
+:end
+echo ending the showtasks
+
