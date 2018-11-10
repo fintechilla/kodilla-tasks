7b6328f360ea14bf15dfd351025eff4aba564cf3
fintechilla <shwalerys@gmail.com>
2018-11-10 19:40:27 +0100

    Module 17.4 - Shell Script - opens up a web page and a localhost. I added 30 secs of a delay.

--- showtasks.bat
+++ showtasks.bat
@@ -1,14 +1,23 @@
 echo starting... showtasks.bat
 echo running... runcrud.bat
 call runcrud.bat
-
 if "%ERRORLEVEL%" == "0" goto startbrowser
 echo There was a problem with runcrud.bat
-goto end
+goto fail
 
 :startbrowser
 START chrome "https://www.onet.pl"
-echo openining webbrowser successfull
+if "%ERRORLEVEL%" == "0" goto startlocalhost
+echo There was a problem with openning a web browser
+goto fail
+
+:startlocalhost
+TIMEOUT /T 30
+START chrome "http://localhost:8080/crud/v1/task/getTasks"
+if "%ERRORLEVEL%" == "0" goto end
+
+:fail
+echo There were some problems - abort execution
 
 :end
 echo ending the showtasks
